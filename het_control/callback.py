97061994285fc1fd5da368682a356c2cf4f96297
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-05-13 11:51:33 +0100

    Update copyright

--- het_control/callback.py
+++ het_control/callback.py
@@ -1,8 +1,6 @@
-#  Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-#  This source code is licensed under the license found in the
-#  LICENSE file in the root directory of this source tree.
-#
+#  Copyright (c) 2024.
+#  ProrokLab (https://www.proroklab.org/)
+#  All rights reserved.
 
 from typing import List
 
